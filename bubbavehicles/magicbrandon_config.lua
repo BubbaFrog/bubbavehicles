2577c4e5f45461bf57866afd5164d27658eff39b
BubbaFrog <100529581+BubbaFrog@users.noreply.github.com>
2022-12-18 17:52:35 -0500

    Add files via upload

---
+++ bubbavehicles/magicbrandon_config.lua
@@ -0,0 +1,7 @@
+bubbavehicles = {}
+
+--If you want Magic Brandon Flags to be available only through /giveme or at admin shops, set this to false.
+bubbavehicles.crafts = true
+
+--If you want Magic Brandon Flags to be only one use, set this to true.
+bubbavehicles.one_use = false
